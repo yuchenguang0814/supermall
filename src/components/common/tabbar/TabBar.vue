ab57e8fe99e53133f2bae13c6589cc4cf0cbec04
Yu <546448821@qq.com>
2020-09-10 16:58:41 +0800

    结构搭建

---
+++ src/components/common/tabbar/TabBar.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="tab-bar">
+      <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TabBar'
+}
+</script>
+
+<style scoped>
+#tab-bar {
+  display: flex;
+  background-color: #fff;
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  box-shadow: 0px -3px 3px rgba(100, 100, 100, 0.3);
+}
+</style>
