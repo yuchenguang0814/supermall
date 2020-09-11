c532b717908f2fafa60b66736f42cee2a5c2eb52
Yu <546448821@qq.com>
2020-09-11 16:41:06 +0800

    首页轮播图

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -8,10 +8,24 @@
 
 <script>
   import NavBar from 'components/common/navbar/NavBar'
+  import {GetHomeMultidata} from 'network/home'
   export default {
     name: "Home",
     components: {
       NavBar
+    },
+    data() {
+      return {
+        banners: null
+      }
+    },
+    created() {
+      GetHomeMultidata().then(res => {
+        this.banners = res.data.banner
+        console.log(this.banners)
+      })
+    
+      
     }
   }
 </script>
